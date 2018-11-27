89e5268e97601fdea2d912e325d131801b4d4e67
Iord Neto <iordneto95@gmail.com>
2018-11-27 17:49:48 -0200

    Updates

---
+++ precache-manifest.2253897d68dd13a4b82f617bb04adcf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jogo-das-cores/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90b4c0e532499e610390",
+    "url": "/jogo-das-cores/static/js/runtime~main.90b4c0e5.js"
+  },
+  {
+    "revision": "112c2cd429e2c135bfad",
+    "url": "/jogo-das-cores/static/js/main.112c2cd4.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/jogo-das-cores/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "112c2cd429e2c135bfad",
+    "url": "/jogo-das-cores/static/css/main.b48683ba.chunk.css"
+  },
+  {
+    "revision": "6a9774508ddcca4c917998a708f232d1",
+    "url": "/jogo-das-cores/index.html"
+  }
+];
