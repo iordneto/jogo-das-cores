96d895f89008dce1b4b11cb3ddfaaca613476de3
Iord Neto <iordneto95@gmail.com>
2018-11-28 16:07:57 -0200

    Add static data that will provides the game

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const wordsAndAnswers = [
+  { word: "maçã", answer: "red" },
+  { word: "banana", answer: "yellow" },
+  { word: "sol", answer: "yellow" },
+  { word: "céu", answer: "blue" },
+  { word: "oceano", answer: "blue" },
+  { word: "grêmio", answer: "blue" },
+  { word: "inter", answer: "red" },
+  { word: "girassol", answer: "yellow" },
+  { word: "cenoura", answer: "orange" },
+  { word: "alface", answer: "green" },
+  { word: "morango", answer: "red" },
+  { word: "limão", answer: "green" },
+  { word: "tomate", answer: "red" },
+  { word: "bob esponja", answer: "yellow" },
+  { word: "smurf", answer: "blue" },
+  { word: "Shrek", answer: "green" },
+  { word: "Simpsons", answer: "yellow" },
+  { word: "Patrick Estrela", answer: "pink" },
+  { word: "Peppa Pig", answer: "pink" },
+  { word: "Minions", answer: "yellow" },
+  { word: "Facebook", answer: "blue" },
+  { word: "Twitter", answer: "blue" },
+  { word: "Coração", answer: "red" },
+  { word: "Arroz", answer: "white" },
+  { word: "Pneu", answer: "black" },
+  { word: "Neve", answer: "white" },
+  { word: "Snapchat", answer: "yellow" },
+  { word: "Galinha Pintadinha", answer: "blue" },
+  { word: "Netflix", answer: "red" },
+  { word: "Spotify", answer: "green" }
+];
+
+export const nameOfColors = [
+  { name: "Vermelho", answer: "red" },
+  { name: "Azul", answer: "blue" },
+  { name: "Amarelo", answer: "yellow" },
+  { name: "Verde", answer: "green" },
+  { name: "Rosa", answer: "pink" },
+  { name: "Branco", answer: "white" },
+  { name: "Preto", answer: "black" },
+  { name: "Laranja", answer: "orange" }
+];
+
+export const colors = [
+  { backGroundColor: "red", textColor: "blue" },
+  { backGroundColor: "blue", textColor: "red" },
+  { backGroundColor: "yellow", textColor: "green" },
+  { backGroundColor: "green", textColor: "black" },
+  { backGroundColor: "pink", textColor: "blue" },
+  { backGroundColor: "white", textColor: "green" },
+  { backGroundColor: "black", textColor: "orange" },
+  { backGroundColor: "orange", textColor: "white" }
+];
